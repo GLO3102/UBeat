770eed19aa8a97c5930a6622597ea63eaec8ad38
William Fortin <willyfortin@gmail.com>
2014-08-05 22:21:38 -0400

    - Revert user agent

--- routes/search.js
+++ routes/search.js
@@ -23,10 +23,7 @@ function search(url, searchQuery, res) {
     });
     request({
             uri: url,
-            method: 'GET',
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1985.125 Safari/537.36'
-            }
+            method: 'GET'
         },
         function (error, response, body) {
             console.log(JSON.stringify(response), JSON.stringify(body));
