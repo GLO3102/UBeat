0ae774e50f1c7871824115145f2893db72611fe8
William Fortin <willyfortin@gmail.com>
2014-08-06 21:21:09 -0400

    * Update dependencies and refactor playlist method names

--- routes/playlists.js
+++ routes/playlists.js
@@ -1,8 +1,8 @@
 var Playlist = require('../models/playlist').model;
 var User = require('../models/user').model;
 
-exports.create = function (req, res) {
-    User.findById('53d847f8bc75352a1c04dcd9', function (err, user) {
+exports.createPlaylist = function (req, res) {
+    User.findPlaylistById('53d847f8bc75352a1c04dcd9', function (err, user) {
         if (!err) {
             var playlist = new Playlist({
                 name: req.body.name,
@@ -21,46 +21,23 @@ exports.create = function (req, res) {
     });
 };
 
-exports.update = function (req, res) {
-    Playlist.findById(req.params.id, function (err, playlist) {
-        if (!err) {
-            if (playlist) {
-                console.log(req.body.tracks);
-                playlist.tracks = req.body.tracks;
-                playlist.save();
-                res.status(200).send(playlist);
-            } else {
-                res.status(404).send({
-                    errorCode: 'PLAYLIST_NOT_FOUND',
-                    message: 'Playlist ' + req.params.id + ' was not found'
-                });
-            }
-        } else {
-            console.error(err);
-            if (err.name === 'CastError') {
-                res.status(404).send({
-                    errorCode: 'PLAYLIST_NOT_FOUND',
-                    message: 'Playlist ' + req.params.id + ' was not found'
-                });
-            } else {
-                res.status(500).send(err);
-            }
-        }
-    });
+exports.addTrack = function (req, res) {
+  Playlist.find()
 };
 
-exports.findAll = function (req, res) {
+exports.getPlaylists = function (req, res) {
     Playlist.find({}, function (err, playlists) {
         if (!err) {
             res.status(200).send(playlists || []);
         } else {
             console.log(err);
+            res.status(500).send(err);
         }
     });
 };
 
-exports.findById = function (req, res) {
-    Playlist.findById(req.params.id, function (err, playlist) {
+exports.findPlaylistById = function (req, res) {
+    Playlist.findPlaylistById(req.params.id, function (err, playlist) {
         if (!err) {
             if (playlist) {
                 res.status(200).send(playlist);
